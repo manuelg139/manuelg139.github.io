908a2b5d23b6ddf70fc19cdddbe748f804336ab4
ManuelGF <manuelg139@gmail.com>
2021-01-15 20:09:33 -0600

    All New Acticities

---
+++ TEST FILES/helloworld.js
@@ -0,0 +1,18 @@
+
+
+
+var name = prompt ("Whats Your Name?");
+    if (name === "Caleb" || "caleb") {
+        alert ("Hello Caleb!")
+    }
+
+    else if (name === "Manuel") {
+        alert ("Hello Manuel")
+    }
+    else if (name === "Damnian") {
+        alert ("Hello Damian")
+    }
+
+    else {
+        alert("Hello Stranger!!")
+    }
